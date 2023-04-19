7ef8e11a183fbde663247a661186f18b705500dc
yury-poloshkov <1286600@google.com>
2023-04-19 15:01:56 +0300

    task24 completed

--- Homework04/task24.py
+++ Homework04/task24.py
@@ -11,3 +11,26 @@
 # собрать за один заход собирающий модуль, находясь перед некоторым кустом
 # заданной во входном файле грядки.
 
+def GenerateRandomIntList():
+    import random 
+    n = int(input("Введите количество кустов: "))
+    min_number = int(input("Введите минимальное количество ягод на кусте: "))
+    max_number = int(input("Введите максимальное количество ягод на кусте: "))
+    numbers = []
+    for i in range(n):
+        numbers.append(random.randint(min_number,max_number))
+    return numbers
+
+garden = GenerateRandomIntList()
+print(garden)
+
+best_point = 0
+best_point_sum = cur_point_sum = garden[-1] + garden[0] + garden[1]
+
+print(best_point_sum)
+for cur_point in range(1, len(garden)):
+    cur_point_sum = cur_point_sum - garden[cur_point-2] + garden[(cur_point+1)%len(garden)]
+    if best_point_sum < cur_point_sum:
+        best_point = cur_point
+        best_point_sum = cur_point_sum
+print(str(best_point+1) + " : " + str(best_point_sum))
